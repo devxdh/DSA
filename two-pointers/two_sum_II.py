ea4edae69c58c14c6e82d36d7090ef0144141675
Dev Dhanadiya <248318636+devxdh@users.noreply.github.com>
2026-09-02 19:13:20 +0530

    two-pointers: 1, 2

---
+++ two-pointers/two_sum_II.py
@@ -0,0 +1,16 @@
+def twoSum(numbers: list[int], target: int) -> list[int]:
+    if not numbers:
+        return []
+
+    l, r = 0, len(numbers) - 1
+    while l < r:
+        sumVal = numbers[l] + numbers[r]
+        if sumVal == target:
+            return [l + 1, r + 1]
+        elif sumVal > target:
+            r -= 1
+        else:
+            l += 1
+
+
+print(twoSum([-1, 0], -1))
